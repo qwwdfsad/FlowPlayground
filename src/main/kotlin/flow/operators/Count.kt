b009032504654bd5d4ede43f071a1617c7688c0b
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-02-04 19:09:31 +0300

    Count operator

---
+++ src/main/kotlin/flow/operators/Count.kt
@@ -0,0 +1,35 @@
+package flow.operators
+
+import flow.*
+
+fun <T> Flow<T>.count(): Flow<Long> = flow {
+    var i = 0L
+    consumeEach {
+        ++i
+    }
+    push(i)
+}
+
+suspend fun main() {
+    println("Example 1: ${ runCatching { example1() }}")
+    println("Example 2: ${ runCatching { example2() }}")
+}
+
+private suspend fun example2() {
+    val f1 = flow(1).delay(5000).map { println("Whoa"); it }
+    val f2 = flow(2).delay(100).map {
+        error(":(")
+        42
+    }
+
+    flow(f1, f2).flatMap { it }.count().consumeEach {
+        println("$it elements")
+    }
+}
+
+
+private suspend fun example1() {
+    flow(1, 2, 3, 4).delayEach(100).count().consumeEach {
+        println("$it elements")
+    }
+}
