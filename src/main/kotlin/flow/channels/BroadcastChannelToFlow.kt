7247f89c4b340f91cf8d59d0bbae2fbffd860eb5
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-02-11 21:22:30 +0300

    flow.asBroadcastChannel

--- src/main/kotlin/flow/channels/BroadcastChannelToFlow.kt
+++ src/main/kotlin/flow/channels/BroadcastChannelToFlow.kt
@@ -1,14 +1,31 @@
 package flow.channels
 
+import flow.*
 import flow.operators.*
 import flow.source.*
 import flow.terminal.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.*
 
-fun <T : Any> BroadcastChannel<T>.flow() = flow<T> {
-    openSubscription().consumeEach {
-        push(it)
+fun <T : Any> BroadcastChannel<T>.asFlow() = flow<T> {
+    val subscription = openSubscription()
+    try {
+        for (element in subscription) {
+            push(element)
+        }
+    }  catch (e: Throwable) {
+        if (e is CancellationException) {
+            subscription.cancel()
+        } else {
+            subscription.cancel(e)
+            throw e
+        }
+    }
+}
+
+fun <T : Any> Flow<T>.asChannel(): BroadcastChannel<T> = GlobalScope.broadcast(Dispatchers.Unconfined) {
+    flowBridge { value ->
+        send(value)
     }
 }
 
@@ -21,7 +38,7 @@ suspend fun main() {
         }
     }
 
-    val flow = bc.flow().limit(10).filter { it % 2 == 0 }
+    val flow = bc.asFlow().limit(10).filter { it % 2 == 0 }
     println("Flow prepared, sleeping for 100 ms")
     delay(100)
 
