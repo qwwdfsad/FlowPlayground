8384b47e746b3d3002964e8848f00f9543db2576
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-02-21 16:10:34 +0300

    Inline collect as well

--- src/jmh/kotlin/benchmarks/FuseableFlow.kt
+++ src/jmh/kotlin/benchmarks/FuseableFlow.kt
@@ -1,60 +1,51 @@
 package benchmarks
 
+import examples.*
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.flow.builders.*
 import kotlinx.coroutines.flow.operators.*
-import reactor.core.publisher.*
-import java.util.*
 
 typealias Transformer<T, R> = suspend (T) -> R?
 
 // TODO it could be non-public
 abstract class FuseableFlow<T : Any, R : Any>(private val flow: Flow<T>) : Flow<R> {
 
-    private val transformers = ArrayDeque<FuseableFlow<Any, Any>>(4)
-    private val sourceFlow: Flow<*> = nonFuseableFlow(this)
+    public val sourceFlow: Flow<*> = nonFuseableFlow(this)
 
     private tailrec fun nonFuseableFlow(flow: Flow<*>): Flow<*> {
         if (flow !is FuseableFlow<*, *>) {
             return flow
         }
 
-        transformers.addFirst(flow as FuseableFlow<Any, Any>)
         return nonFuseableFlow(flow.flow)
     }
 
     protected abstract suspend fun transform(value: T): R?
 
-    override suspend fun collect(collector: FlowCollector<R>) {
-        if (sourceFlow === flow) {
-            flow.collect { value ->
-                val actual = transform(value) ?: return@collect
-                collector.emit(actual)
-            }
-            return
+    public suspend fun transformChained(value: Any): R? {
+        if (flow is FuseableFlow<*, *>) {
+            val tr = flow.transformChained(value) ?: return null
+            return transform(tr as T)
+        } else {
+            return transform(value as T)
         }
-
-        sourceFlow.collect { value ->
-            var transformed = value
-            for (fuseable in transformers) {
-                transformed = fuseable.transform(transformed) ?: return@collect
-            }
-
-            collector.emit(transformed as R)
-        }
-
     }
 }
 
 inline fun <T : Any, R : Any> Flow<T>.transform(crossinline transformer: Transformer<T, R>): Flow<R> =
     object : FuseableFlow<T, R>(this) {
-        override suspend fun transform(value: T): R? {
-            return transformer(value)
-        }
+        override suspend fun transform(value: T): R? = transformer(value)
+
+        override suspend fun collect(collector: FlowCollector<R>)=
+            sourceFlow.collect {
+                val value = transformChained(it)
+                if (value != null) collector.emit(value)
+            }
     }
 
 inline fun <T : Any> Flow<T>.fusedFilter(crossinline predicate: suspend (T) -> Boolean) =
-    transform() { if (predicate(it)) it else null }
+    transform { if (predicate(it)) it else null }
+
 
 inline fun <T : Any, R : Any> Flow<T>.fusedMap(
     crossinline transform: suspend (T) -> R
@@ -71,7 +62,7 @@ private val source = flow {
 
 
 private val fusedInlineSource = flow {
-    repeat(10_000) {
+    repeat(1) {
         emit(it)
     }
 }.fusedMap() { it + 1 }
@@ -79,8 +70,5 @@ private val fusedInlineSource = flow {
     .fusedFilter() { true }
 
 suspend fun main() {
-    Flux.range(0, 10_000).map { it + 1 }
-        .map { it - 1 }
-        .filter() { true }
-        .blockLast()
+    println(fusedInlineSource.sum())
 }
